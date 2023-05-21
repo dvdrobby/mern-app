0f5a8ce25a8cdeeb123e3f6a9dd9aac76435f192
dvdrobby <david.robby.sanjaya@gmail.com>
2023-05-22 06:24:13 +0700

    styling header & footer

--- src/pages/mainapp/index.js
+++ src/pages/mainapp/index.js
@@ -3,20 +3,26 @@ import {Routes as Switch, Route } from 'react-router-dom'
 import About from '../about'
 import Post from '../post'
 import Home from '../home'
+import { Header, Footer } from '../../components'
 
 
 function MainApp() {
   return (
-    <>
-    <header>header</header>
-    <Switch>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/post" element={<Post />} />
-    </Switch>
- 
-    <footer>footer</footer>
-    </>
+    <div className='d-flex flex-column vh-100'>
+        <div>
+            <Header />
+        </div>
+        <div className='flex-grow-1 container'>
+            <Switch>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/post" element={<Post />} />
+            </Switch>
+        </div>
+        <div className='text-bg-dark fluid p-3'>
+            <Footer />
+        </div>
+    </div>
   )
 }
 
